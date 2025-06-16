58b2b96f94db4f3ebc166c32afc9aae3d409ba66
Ali Shakiba <alishakiba@users.noreply.github.com>
2025-06-16 09:23:00 +0000

    notebook to script

--- exercises/week1/setup.sh
+++ exercises/week1/setup.sh
@@ -11,3 +11,32 @@
 !mv green_tripdata_2021-02.parquet ../data/
 # Running mlflow
 mlflow ui --backend-store-uri sqlite:///mlflow.db
+# Running mlflow to track locally - single developer
+mlflow ui --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./artifacts
+######################################
+
+# mlflow.dockerfile
+
+FROM python:3.10-slim
+
+RUN pip install mlflow
+
+EXPOSE 5000
+
+CMD [ \
+    "mlflow", "server", \
+    "--backend-store-uri", "sqlite:///home/mlflow_data/mlflow.db", \
+    "--host", "0.0.0.0", \
+    "--port", "5000" \
+]
+
+# docker-compose.yaml
+mlflow:
+    build:
+        context: .
+        dockerfile: mlflow.dockerfile
+    ports:
+        - "5000:5000"
+    volumes:
+        - "${PWD}/mlflow_data:/home/mlflow_data/"
+
